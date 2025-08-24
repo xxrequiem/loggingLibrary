6d555b72b6e9bbd6ea8de0dc9d26265b6db3d29e
xxrequiem <danilvd1337@gmail.com>
2025-08-24 03:26:53 +0300

    adding a constructor and a destructor, correcting comments

--- src/logger.cpp
+++ src/logger.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-string logLevelToString(logLevel level) {  // перевод уровня логирования в строку
+string logLevelToString(logLevel level) {  // перевод "уровня логирования" в строку
     switch (level) {
         case logLevel::INFO:
             return "INFO";
@@ -18,5 +18,18 @@ string logLevelToString(logLevel level) {  // перевод уровня лог
             return "ERROR";
     }
     
-    throw invalid_argument("Error: Incorrect level value!");
+    throw invalid_argument("Error: Incorrect level value!");     // исключение, если выбран некорректный уровень логирования
+}
+
+logger::logger(const& string filename, logLevel level) {         // конструктор для инициализации потока и установки уровня логирования
+    logFile.open(filename, ios::app, ios::ate);                  // открытие файла для дозаписи
+    if (!logFile.is_open()) {                                    // проверка корректности открытия файла
+        throw runtime_error("Error: Failed to open the file!");  // исключение, если файл не удалось открыть
+    }
+}
+
+logger::~logger() {           // деструктор для закрытия файла
+    if (logFile.is_open()) {  // проверка, открыт ли файл
+        logFile.close();      // закрытие файла
+    }
 }
